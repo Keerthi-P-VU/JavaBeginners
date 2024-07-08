54d30c5fc6d4e7412ae97b8478856dd9e82dca1d
KEERTHI <KEERTHI@DESKTOP-CI1HS7J>
2024-07-08 22:16:17 +0530

    Initial Commit

---
+++ src/BasicProgramming/Add2Nums.java
@@ -0,0 +1,17 @@
+package BasicProgramming;
+
+import java.util.*;
+
+public class Add2Nums {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter value of a:");
+	int a = sc.nextInt();
+	System.out.println("Enter value of b:");
+	int b = sc.nextInt();
+	System.out.println("Sum of two nums is :"+ (a+b));
+	}
+
+}
